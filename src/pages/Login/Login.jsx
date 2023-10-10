be6388c66d87580f81b2de3152949b9668c27609
EFFM00 <fischietto.elena@gmail.com>
2023-10-10 00:59:14 -0300

    creating login component

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+
+const Login = () => {
+    return (
+        <>
+        <input type="email"/>
+        <input type="password"/>
+        <button>Login</button>
+        </>
+    )
+}
+
+export default Login
