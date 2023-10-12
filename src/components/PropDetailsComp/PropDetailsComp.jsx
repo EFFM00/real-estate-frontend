6d2c52d749264a45d8860efda5ceee71af1f9b65
EFFM00 <fischietto.elena@gmail.com>
2023-10-12 06:47:52 -0300

    gallery resolved

--- src/components/PropDetailsComp/PropDetailsComp.jsx
+++ src/components/PropDetailsComp/PropDetailsComp.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useCatalogue } from '../../context/CatalogueProvider';
-import axios from 'axios';
 import { PropsContainer } from './style';
 import Gallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import axios from 'axios';
+import { useCatalogue } from '../../context/CatalogueProvider';
+import { useParams } from 'react-router-dom';
 
 const PropDetailsComp = () => {
 
@@ -15,59 +15,48 @@ const PropDetailsComp = () => {
 
     useEffect(() => {
 
-        console.log("ADSADAS", images);
+        const url = "https://api-real-estates.onrender.com/api/properties/" + propertyId
 
-    }, [images])
+        const token = localStorage.getItem("token");
 
-
-    useEffect(() => {
-
-        if(Object.keys(propsDetails).length === 0) {
-            const url = "https://api-real-estates.onrender.com/api/properties/" + propertyId
-
-            const token = localStorage.getItem("token");
-
-            const headers = {
-                headers: {
-                    "Authorization": "Bearer " + JSON.parse(token)
-                }
+        const headers = {
+            headers: {
+                "Authorization": "Bearer " + JSON.parse(token)
             }
-            axios.get(url, headers)
-            .then(res => {
-                setPropDetails(res.data.data)
-
-                const arrImg = res.data.data.images.map(img => {
-                    return {
-                        original: img.url_image,
-                        thumbnail: img.url_image,
-                    }
-                })
-
-                const arrImgFinal = [
-                    {
-                        original: res.data.data.main_image,
-                        thumbnail: res.data.data.main_image,
-                    },
-                    ...arrImg
-                ]
-                console.log("AAAAAA", arrImgFinal);
-
-                setImages(arrImgFinal);
-            }).catch(err => {
-                console.log(err);
-            })
         }
+        
+        axios.get(url, headers)
+        .then(res => {
+            setPropDetails(res.data.data)
+
+            const arrImg = res.data.data.images.map(img => {
+                return {
+                    original: img.url_image,
+                    thumbnail: img.url_image,
+                }
+            })
+
+            const arrImgFinal = [
+                {
+                    original: res.data.data.main_image,
+                    thumbnail: res.data.data.main_image,
+                },
+                ...arrImg
+            ]
+
+            setImages(arrImgFinal);
+        }).catch(err => {
+            console.log(err);
+        })
 
     }, [])
-
 
     return(
         propsDetails && 
         <PropsContainer>
             <div>
                 <h1>{propsDetails.title}</h1>
-                <Gallery itemWidth={40} itemHeight={34} items={images} />        
-
+                {images && <Gallery key={images.length} itemWidth={40} itemHeight={34} items={images} />}
             </div>
         </PropsContainer>
     )
--- src/pages/PropDetails/PropDetails.jsx
+++ src/pages/PropDetails/PropDetails.jsx
@@ -2,12 +2,16 @@ import FormMessage from '../../components/FormMessage/FormMessage';
 import { DetailsContainer } from './style';
 import { ButtonLogout } from '../../components/CatalogueComp/styled';
 import PropDetailsComp from '../../components/PropDetailsComp/PropDetailsComp';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthProvider';
 
 const PropDetails = () => {
 
+    const navigate = useNavigate();
+    const {setLogged} = useAuth();
+
 
     const handleLogout = () => {
-
         localStorage.removeItem("token");
         setLogged(false);
         navigate("/")
@@ -16,7 +20,7 @@ const PropDetails = () => {
     return(
         <DetailsContainer>
             <ButtonLogout onClick={handleLogout}>Log out</ButtonLogout>
-            <PropDetailsComp />
+            <PropDetailsComp/>
             <FormMessage/>
         </DetailsContainer>
     )
