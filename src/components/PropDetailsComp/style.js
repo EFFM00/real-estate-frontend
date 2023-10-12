bdeaafe57c74911ad0100d7d2340613210eaa654
EFFM00 <fischietto.elena@gmail.com>
2023-10-12 09:05:00 -0300

    done

--- src/components/PropDetailsComp/style.js
+++ src/components/PropDetailsComp/style.js
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
-export const PropsContainer = styled.div`
+export const DetailsCont = styled.div`
+    margin: 5rem auto 5rem auto;
+    color: gray;
+`
 
+export const GalleryCont = styled.div`
+    max-width: 800px;
+    margin: 0 auto 2rem auto;
+`
+
+export const Address = styled.h2`
+    font-size: 1.5rem;
 `
--- src/pages/PropDetails/style.js
+++ src/pages/PropDetails/style.js
@@ -4,6 +4,8 @@ export const DetailsContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr;
     gap: 5rem;
+    width: 80%;
+    margin: 0 auto;
 
     @media (min-width: 768px) {
         grid-template-columns: 3fr 1fr;
